a70374640db210da301c2752fbadad835c116374
jshinds4 <jshinds4@M5290478.Go.JohnsonControls.com>
2021-07-17 20:37:21 +0530

    test Class 4 message

---
+++ TestNG_Demo/src/com/testCases/TestClass4.java
@@ -0,0 +1,12 @@
+package com.testCases;
+
+public class TestClass4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub.
+		
+		System.out.println(" This is new class");
+
+	}
+
+}
