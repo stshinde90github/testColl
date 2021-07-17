02a273ae8695f64fc1a951136da5cfdd1754dd56
jshinds4 <jshinds4@M5290478.Go.JohnsonControls.com>
2021-07-17 20:26:05 +0530

    New project as CheckGitFun

---
+++ CheckGitFun/src/com/testCases/TestClass1.java
@@ -0,0 +1,12 @@
+package com.testCases;
+
+public class TestClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("first class");
+
+	}
+
+}
